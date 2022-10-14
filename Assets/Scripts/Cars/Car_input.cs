5c40c690c85db853998eddfe79c8b494b3b75b9d
Neroxgd <darkaux69@gmail.com>
2022-10-14 02:58:33 +0200

    tourner

--- Assets/Scripts/Cars/Car_input.cs
+++ Assets/Scripts/Cars/Car_input.cs
@@ -1,19 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Car_input : MonoBehaviour
 {
     private Rigidbody _rigidebody;
-    [SerializeField] [Range(1, 1000)] private int speedCar = 5;
+    [SerializeField][Range(1, 100)] private int speedCar = 5;
+    [SerializeField][Range(1, 5)] private int speed_rotate = 5;
+    [SerializeField] private InputAction _inputeAction;
+    private void OnEnable() { _inputeAction.Enable(); }
+    private void OnDisable() { _inputeAction.Disable(); }
+    private float _rotation = 0;
     void Start()
     {
         _rigidebody = GetComponent<Rigidbody>();
+
+    }
+
+    void Update()
+    {
+        _rotation = _inputeAction.ReadValue<Vector2>().x;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        _rigidebody.AddForce(Vector3.forward*speedCar*Time.deltaTime);
+        transform.Rotate(new Vector3(0, _rotation, 0) * speed_rotate, Space.Self);
+        _rigidebody.velocity = transform.right * speedCar;
+        Debug.Log(_rotation);
     }
 }
