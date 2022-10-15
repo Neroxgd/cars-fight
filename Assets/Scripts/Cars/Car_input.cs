a4c32486d2a214619a540dcd977207d4b1c1ea52
Neroxgd <darkaux69@gmail.com>
2022-10-15 02:57:21 +0200

    wheels ptn

--- Assets/Scripts/Cars/Car_input.cs
+++ Assets/Scripts/Cars/Car_input.cs
@@ -8,10 +8,13 @@ public class Car_input : MonoBehaviour
     private Rigidbody _rigidebody;
     [SerializeField][Range(1, 100)] private int speedCar = 5;
     [SerializeField][Range(1, 5)] private int speed_rotate = 5;
-    [SerializeField] private InputAction _inputeAction;
-    private void OnEnable() { _inputeAction.Enable(); }
-    private void OnDisable() { _inputeAction.Disable(); }
+    [SerializeField] private InputAction _inputedirection;
+    // [SerializeField] private InputAction _inputeforce;
+    private void OnEnable() { _inputedirection.Enable(); }
+    private void OnDisable() { _inputedirection.Disable(); }
     private float _rotation = 0;
+    [SerializeField] private int puissance = 0;
+    private bool inHold = false;
     void Start()
     {
         _rigidebody = GetComponent<Rigidbody>();
@@ -20,14 +23,37 @@ public class Car_input : MonoBehaviour
 
     void Update()
     {
-        _rotation = _inputeAction.ReadValue<Vector2>().x;
+        _rotation = _inputedirection.ReadValue<Vector2>().x;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        /*if (Keyboard.current.wKey.isPressed)
+        {
+            puissance++;
+            inHold = true;
+        }
+        else if (Keyboard.current.sKey.isPressed)
+        {
+            puissance--;
+            inHold = true;
+        }
+        else
+        {
+            inHold = false;
+            addForce();
+        }
         transform.Rotate(new Vector3(0, _rotation, 0) * speed_rotate, Space.Self);
-        _rigidebody.velocity = transform.right * speedCar;
-        Debug.Log(_rotation);
+        Vector3 _vecocity = _rigidebody.velocity;
+        _rigidebody.velocity = Vector3.zero;
+        _rigidebody.AddForce(_vecocity + (new Vector3(_rotation, 0, 0) * speed_rotate), ForceMode.Impulse);
+        Debug.Log(_rigidebody.velocity);*/
     }
+
+    /*public void addForce()
+    {
+        _rigidebody.AddForce(transform.right * puissance, ForceMode.Impulse);
+        puissance = 0;
+    }*/
 }
