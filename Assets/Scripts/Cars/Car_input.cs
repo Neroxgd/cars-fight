5b100d75464a2146fa43890ae2341cfb982d083c
Neroxgd <darkaux69@gmail.com>
2022-10-15 15:52:14 +0200

    freeze rotation disabled

--- Assets/Scripts/Cars/Car_input.cs
+++ Assets/Scripts/Cars/Car_input.cs
@@ -13,13 +13,8 @@ public class Car_input : MonoBehaviour
     [SerializeField] private WheelCollider _FL;
     [SerializeField] private WheelCollider _BR;
     [SerializeField] private WheelCollider _BL;
-    // [SerializeField] private float acceleration = 500f;
-    // [SerializeField] private float breakingForce = 300f;
     [SerializeField] private float maxTurnAngle = 45f;
-    // private float currentAcceleration = 0;
-    // private float currentBreakForce = 0;
     private float currentTurnAngle = 0;
-    // [SerializeField] private InputAction _inputeforce;
     private void OnEnable() { _inputedirection.Enable(); }
     private void OnDisable() { _inputedirection.Disable(); }
     private float _rotation = 0;
@@ -28,34 +23,24 @@ public class Car_input : MonoBehaviour
     void Start()
     {
         _rigidebody = GetComponent<Rigidbody>();
-        _rigidebody.AddForce(transform.forward*1000, ForceMode.Impulse);
+        //debug the collision when you spawn
+        _rigidebody.AddForce(transform.forward*5000, ForceMode.Impulse);
     }
 
     void Update()
     {
+        //get the horizontal axe input values
         _rotation = _inputedirection.ReadValue<Vector2>().x;
     }
 
-    // Update is called once per frame
     void FixedUpdate()
     {
-        /*currentAcceleration = acceleration * Input.GetAxis("Vertical");
-
-        if (Keyboard.current.spaceKey.isPressed)
-            currentBreakForce = breakingForce;
-        else
-            currentBreakForce = 0;
-        _FR.motorTorque = currentAcceleration;
-        _FL.motorTorque = currentAcceleration;
-
-        _FR.brakeTorque = currentBreakForce;
-        _FL.brakeTorque = currentBreakForce;
-        _BR.brakeTorque = currentBreakForce;
-        _BL.brakeTorque = currentBreakForce;*/
-
+        //turn the car
         currentTurnAngle = maxTurnAngle * _rotation;
         _FL.steerAngle = currentTurnAngle;
         _FR.steerAngle = currentTurnAngle;
+
+        //increase the puissance
         if (Keyboard.current.wKey.isPressed)
         {
             puissance += speedCar;
@@ -71,19 +56,12 @@ public class Car_input : MonoBehaviour
             inHold = false;
             addForce();
         }
-        // transform.Rotate(new Vector3(0, _rotation, 0) * speed_rotate, Space.Self);
-        // Vector3 _vecocity = _rigidebody.velocity;
-        // _rigidebody.velocity = Vector3.zero;
-        // _rigidebody.AddForce(_vecocity + (new Vector3(_rotation, 0, 0) * speed_rotate), ForceMode.Impulse);
-        Debug.Log(_FR.motorTorque);
     }
 
+    //apply the instante force
     public void addForce()
     {
         _rigidebody.AddForce(transform.forward * puissance, ForceMode.Impulse);
-        
-        // _FR.brakeTorque = puissance;
-        // _FL.brakeTorque = puissance;
         puissance = 0;
     }
 }
---
+++ Assets/Scripts/Cars/Car_ragdoll_active.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car_ragdoll_active : MonoBehaviour
+{
+    [SerializeField] private Rigidbody _rbParent;
+    [SerializeField] private Collider _colliderParent;
+
+    void OnTriggerEnter(Collider other)
+    {
+        //disabled freez rotation on parent
+        _rbParent.constraints = RigidbodyConstraints.None;
+    }
+    void Awake()
+    {
+        //ignore collider collision with parent
+        Physics.IgnoreCollision(GetComponent<Collider>(), _colliderParent);
+    }
+}
