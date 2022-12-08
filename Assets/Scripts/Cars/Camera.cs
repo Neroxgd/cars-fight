6000000113d66b2d9adb4372443851ccd5761ec4
Neroxgd <darkaux69@gmail.com>
2022-12-08 20:13:34 +0100

    smoothcam fixed finalllyyyy

--- Assets/Scripts/Cars/Camera.cs
+++ Assets/Scripts/Cars/Camera.cs
@@ -9,15 +9,25 @@ public class Camera : MonoBehaviour
     [SerializeField] private Rigidbody Cam_rb;
     private Vector3 vit_rot = Vector3.zero;
     [SerializeField] private float hauteurMaxCam = 10;
+    [SerializeField] private float distanceCam;
+    [SerializeField] private float speedCam;
+    private float smoothCam = 1;
 
     //cam follow the car
     void LateUpdate()
     {
         transform.position = _PlayerPosition.position;
-        if (Vector3.Distance(CamPosition.position, transform.position) > 3)
-            Cam_rb.velocity = Cam_rb.transform.forward * Vector3.Distance(CamPosition.position, transform.position) * Time.deltaTime * 250;
-        else 
-            Cam_rb.velocity = Vector3.zero;
+        if (Vector3.Distance(CamPosition.position, transform.position) > distanceCam)
+        {
+            smoothCam = 1;
+            Cam_rb.velocity = Cam_rb.transform.forward * Vector3.Distance(CamPosition.position, transform.position) * Time.deltaTime * speedCam;
+        }
+        else
+        {
+            smoothCam -= 0.1f * Time.deltaTime;
+            Cam_rb.velocity = Vector3.Lerp(Vector3.zero, Cam_rb.velocity, smoothCam);
+        }
+        smoothCam = Mathf.Clamp(smoothCam, 0, 1);
         CamPosition.position = new Vector3(CamPosition.position.x, Mathf.Clamp(CamPosition.position.y, hauteurMaxCam, int.MaxValue), CamPosition.position.z);
         CamPosition.LookAt(transform.position);
     }
