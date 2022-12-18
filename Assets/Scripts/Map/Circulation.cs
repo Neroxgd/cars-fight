276d84187c7acb55fa609ae847d6dc48e2be3299
Neroxgd <darkaux69@gmail.com>
2022-12-18 23:06:19 +0100

    circu1

--- Assets/Scripts/Map/Circulation.cs
+++ Assets/Scripts/Map/Circulation.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class Circulation : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private int timeStart;
+    [SerializeField] private Transform[] intersection;
+
     void Start()
     {
+        StartCoroutine(TimeStart());
+    }
+
+    void _Circulation()
+    {
+        var sequence = DOTween.Sequence();
+        foreach (var _intersection in intersection)
+        {
+            sequence.Append(transform.DOLookAt(_intersection.position, 0.5f));
+            sequence.Append(transform.DOMove(_intersection.position, 1));
+            
+            sequence.SetLoops(-1, LoopType.Restart);
+        }
         
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator TimeStart()
     {
-        
+        yield return new WaitForSeconds(timeStart);
+        _Circulation();
     }
 }
