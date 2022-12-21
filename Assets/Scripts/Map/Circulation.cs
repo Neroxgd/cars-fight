3e818080de6f1735af31aeb132144f93d6615654
Neroxgd <darkaux69@gmail.com>
2022-12-21 02:44:12 +0100

    SPLINE C LA VIE

--- Assets/Scripts/Map/Circulation.cs
+++ Assets/Scripts/Map/Circulation.cs
@@ -7,7 +7,6 @@ public class Circulation : MonoBehaviour
 {
     [SerializeField] private int timeStart;
     [SerializeField] private Transform[] intersection;
-    private List<Vector3> intersectionPoint = new List<Vector3>();
     private bool SmoothIntersection = false;
     private int compterintersection = 0;
     private float InterpolateAmount;
@@ -17,57 +16,43 @@ public class Circulation : MonoBehaviour
     {
         StartCoroutine(TimeStart());
         transform.LookAt(intersection[1].position);
-        foreach (var _intersection in intersection)
-            intersectionPoint.Add(_intersection.position);
     }
 
     void _Circulation()
     {
-        /*if (Vector3.Distance(transform.position, intersection[compterintersection].position) < 3)
-        {
-            compterintersection++;
-            
-        }
-        else
-            transform.position = Vector3.MoveTowards(transform.position, intersection[compterintersection].position, speed * Time.deltaTime);
-        if (Vector3.Distance(transform.position, intersection[intersection.Length - 1].position) < 3)
-        {
-            compterintersection = 0;
-            transform.position = intersection[0].position;
-        }*/
-        InterpolateAmount = (InterpolateAmount + Time.deltaTime) % 1;
-        transform.position = GenerateSpline(intersectionPoint.ToArray(), InterpolateAmount);
+        List<Vector3> intersectionPoint = new List<Vector3>();
+        foreach (var _intersection in intersection)
+            intersectionPoint.Add(_intersection.position);
+        InterpolateAmount = (InterpolateAmount + (Time.deltaTime * speed)) % 1;
+        Vector3 Spline = GenerateSpline(intersectionPoint, InterpolateAmount);
+        transform.LookAt(Spline);
+        transform.position = Spline;
     }
 
-    private Vector3 GenerateSpline(Vector3[] intersections, float t)
+    private Vector3 GenerateSpline(List<Vector3> intersections, float t)
     {
         List<Vector3> ListCCI = new List<Vector3>();
-        Vector3 CalculCurrentIntersection = intersections[0];
-        for (int i = 0; i < intersections.Length; i++)
+        if (intersections.Count == 1)
+            return intersections[0];
+
+        Vector3 CalculCurrentIntersection;
+        for (int i = 0; i < intersections.Count - 1; i++)
         {
-            CalculCurrentIntersection = Vector3.Lerp(CalculCurrentIntersection, intersections[i], t);
+            CalculCurrentIntersection = Vector3.Lerp(intersections[i], intersections[i + 1], t);
             ListCCI.Add(CalculCurrentIntersection);
         }
-        if (ListCCI.Count > 1)
-            return GenerateSpline(ListCCI.ToArray(), t);
-        return ListCCI[0];
+        return GenerateSpline(ListCCI, t);
     }
 
     void Update()
     {
-        _Circulation();
-        /*Vector3 direction = intersection[compterintersection + 1].position - transform.position;
-        Quaternion rotation = Quaternion.LookRotation(direction);
-        if (Vector3.Distance(transform.position, intersection[compterintersection].position) < 3 && compterintersection > 0)
-            transform.rotation = Quaternion.Lerp(transform.rotation, rotation, speed * Time.deltaTime);*/
-
-
+        if (SmoothIntersection)
+            _Circulation();
     }
 
     IEnumerator TimeStart()
     {
         yield return new WaitForSeconds(timeStart);
-        _Circulation();
         SmoothIntersection = true;
     }
 }
