fd48acffa5fe5557b462b571ce5627b296392391
Neroxgd <darkaux69@gmail.com>
2022-12-21 01:05:51 +0100

    spline 1

--- Assets/Scripts/Map/Circulation.cs
+++ Assets/Scripts/Map/Circulation.cs
@@ -7,20 +7,23 @@ public class Circulation : MonoBehaviour
 {
     [SerializeField] private int timeStart;
     [SerializeField] private Transform[] intersection;
-    [SerializeField] private AnimationCurve animationCurve;
+    private List<Vector3> intersectionPoint = new List<Vector3>();
     private bool SmoothIntersection = false;
     private int compterintersection = 0;
+    private float InterpolateAmount;
     [SerializeField] private float speed;
 
     void Start()
     {
         StartCoroutine(TimeStart());
         transform.LookAt(intersection[1].position);
+        foreach (var _intersection in intersection)
+            intersectionPoint.Add(_intersection.position);
     }
 
     void _Circulation()
     {
-        if (Vector3.Distance(transform.position, intersection[compterintersection].position) < 3)
+        /*if (Vector3.Distance(transform.position, intersection[compterintersection].position) < 3)
         {
             compterintersection++;
             
@@ -31,16 +34,34 @@ public class Circulation : MonoBehaviour
         {
             compterintersection = 0;
             transform.position = intersection[0].position;
+        }*/
+        InterpolateAmount = (InterpolateAmount + Time.deltaTime) % 1;
+        transform.position = GenerateSpline(intersectionPoint.ToArray(), InterpolateAmount);
+    }
+
+    private Vector3 GenerateSpline(Vector3[] intersections, float t)
+    {
+        List<Vector3> ListCCI = new List<Vector3>();
+        Vector3 CalculCurrentIntersection = intersections[0];
+        for (int i = 0; i < intersections.Length; i++)
+        {
+            CalculCurrentIntersection = Vector3.Lerp(CalculCurrentIntersection, intersections[i], t);
+            ListCCI.Add(CalculCurrentIntersection);
         }
+        if (ListCCI.Count > 1)
+            return GenerateSpline(ListCCI.ToArray(), t);
+        return ListCCI[0];
     }
 
     void Update()
     {
         _Circulation();
-        Vector3 direction = intersection[compterintersection + 1].position - transform.position;
+        /*Vector3 direction = intersection[compterintersection + 1].position - transform.position;
         Quaternion rotation = Quaternion.LookRotation(direction);
         if (Vector3.Distance(transform.position, intersection[compterintersection].position) < 3 && compterintersection > 0)
-            transform.rotation = Quaternion.Lerp(transform.rotation, rotation, speed * Time.deltaTime);
+            transform.rotation = Quaternion.Lerp(transform.rotation, rotation, speed * Time.deltaTime);*/
+
+
     }
 
     IEnumerator TimeStart()
@@ -48,11 +69,5 @@ public class Circulation : MonoBehaviour
         yield return new WaitForSeconds(timeStart);
         _Circulation();
         SmoothIntersection = true;
-    }
-
-    IEnumerator Turn()
-    {
-        yield return new WaitForSeconds(0.5f);
-        transform.Translate(transform.forward * speed * Time.deltaTime);
     }
 }
